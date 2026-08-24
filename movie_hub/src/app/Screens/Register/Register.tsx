0e879ce71b8d4e8a02ee579e53198d4ef0867569
Gabriel Barboza Palmieri <palmieri.barboza@gmail.com>
2026-08-24 09:51:50 -0300

    Teste

---
+++ movie_hub/src/app/Screens/Register/Register.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import { redirect } from "next/navigation"
+
+export default function Home(){
+    return(
+        <View>
+            <Text> olá mundo</Text>
+        </View>
+       
+    )
+}
+
